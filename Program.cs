ab6adf818de230ad7165f961560d9e01fa799729
Eyad Hamza <Eyadhamza000@gmail.com>
2023-05-04 20:19:21 +0200

    Done

--- Program.cs
+++ Program.cs
@@ -1,20 +1,34 @@
 ﻿
 namespace ConsoleApp1;
 
-internal class Program
+internal static class Program
 {
-    private static List<double> _myData = new List<double>() { 7, 4, 5, 6, 3, 8, 10 };
+    private static readonly List<double> MyData = new() { 7, 4, 5, 6, 3, 8, 10 };
     
-    static void Main(string[] args)
+    static void Main()
     {
-        _myData
+        MyData
             .Select(AddOne)
             .Select(Square)
             .Select(SubtractTen)
             .ToList()
             .ForEach(Console.WriteLine);
         
+        MyData
+            .Select(AddOneSquareSubtractTen())
+            .ToList()
+            .ForEach(Console.WriteLine);
+
         Console.ReadLine();
+    }
+
+    private static Func<double, double> AddOneSquareSubtractTen()
+    {
+        Func<double, double> addOne = AddOne;
+        Func<double, double> square = Square;
+        Func<double, double> subtractTen = SubtractTen;
+        
+        return addOne.Compose(square).Compose(subtractTen);
     }
 
     private static double AddOne(double x)
@@ -28,5 +42,10 @@ internal class Program
     private static double SubtractTen(double x)
     {
         return x - 10;
+    }
+
+    private static Func<T1, T3> Compose<T1, T2, T3>(this Func<T1, T2> f, Func<T2, T3> g)
+    {
+        return x => g(f(x));
     }
 }
